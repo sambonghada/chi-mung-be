b30afb10cfce290287df79d6de5e35b3079991bd
limzzum <chfhddl1234@naver.com>
2024-08-05 22:07:46 +0900

    :construction_worker: Setting : 프로젝트 init

---
+++ src/main/java/com/sambong/chi_mung/ChiMungApplication.java
@@ -0,0 +1,13 @@
+package com.sambong.chi_mung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiMungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiMungApplication.class, args);
+	}
+
+}
